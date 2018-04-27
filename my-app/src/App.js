b5b228201840c9e9a8844b41830d7560183bdd03
EscapeMaster <em_haodong@163.com>
2018-04-27 21:48:38 +0800

    引入redux 和react-redux

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,15 +1,33 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {createStore} from 'redux';
+import { Button } from 'antd-mobile';
+import { connect } from 'react-redux';
+import { addGun, addGunAsync } from './redux';
+
+// const mapStatetoProps = (state)=>{
+//   return {num:state}
+// }
+// const actionCreators = { addGun, addGunAsync };
+// App = connect(mapStatetoProps, actionCreators)(App)
+@connect(
+  state=>({num:state}),
+  { addGun, addGunAsync }
+)
+
 class App extends Component {
   render() {
+    const addGun = this.props.addGun;
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
+        <Button type="primary" onClick={ this.props.addGun}>申请武器</Button>
+        <Button type="primary" onClick={ this.props.addGunAsync}>拖两天再给</Button>
+        <Button type="primary">现在有机枪{this.props.num}把</Button>
+        
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,7 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { createStore, applyMiddleware, compose } from 'redux';
+import { counter } from './redux';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
+import {BrowserRouter, Route, Link} from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const store = createStore(counter, compose(
+    applyMiddleware(thunk),
+    window.devToolsExtension ? window.devToolsExtension() : f=>f
+));
+
+ReactDOM.render(
+    (<Provider store={store}>
+        <BrowserRouter>
+            <ul>
+                Link
+                <Link></Link>
+            </ul>
+            <App />
+        </BrowserRouter>
+    </Provider>), document.getElementById('root')
+);
