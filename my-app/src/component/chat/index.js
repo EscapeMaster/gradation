b9e3041b3aea6d0af5dad1beec7ba3ac142afd1f
EscapeMaster <em_haodong@163.com>
2018-05-18 18:56:05 +0800

    emoji表情的制作

--- my-app/src/component/chat/index.js
+++ my-app/src/component/chat/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import io from 'socket.io-client';
-import { List, InputItem, NavBar, Icon } from 'antd-mobile';
+import { List, InputItem, NavBar, Icon, Grid } from 'antd-mobile';
 import { spawn } from 'child_process';
 import { connect } from 'react-redux';
 import { getMsgList, sendMsg, recvMsg, getMeMsg } from '../../redux/chat_redux';
@@ -17,15 +17,16 @@ class Chat extends React.Component {
         super(props);
         this.state = {
             text: '',
-            msg: []
+            msg: [],
+            showEmoji: false
         }
     }
     componentDidMount() {
-        if(!this.props.chat.chatmsg.length){
+        if (!this.props.chat.chatmsg.length) {
             this.props.getMsgList();
             this.props.recvMsg();
         }
-        
+
         // socket.on('recvmsg', (data) => {
         //     this.setState({
         //         msg: [...this.state.msg, data.text]
@@ -38,18 +39,30 @@ class Chat extends React.Component {
         const to = this.props.match.params.user;
         const msg = this.state.text;
         this.props.sendMsg({ from, to, msg });
-        this.setState({ text: '' })
+        this.setState({
+            text: '',
+            showEmoji: false
+        })
+    }
+    fixCarousel() {
+        setTimeout(() => {
+            window.dispatchEvent(new Event('resize'))
+        }, 0)
     }
     render() {
+        const emojiArr = '😀 😁 😂 😃 😄 😅 😆 😉 😊 😋 😎 😍 😘 😗 😙 😚 😇 😐 😑 😶 😏 😣 😥 😮 😯 😪 😫 😴 😌 😛 😜 😝 😒 😓 😔 😕 😲 😷 😖 😞 😟 😤 😢 😭 😦 😧 😨 😬 😰 😱 😳 😵 😡 😠 💪 👈 👉 ☝ 👆 👇 ✌ ✋ 👌 👍 👎 ✊ 👊 👋 👏 👐 ✍ 👦 👧 👨 👩 👴 👵 👶 👱 👮 👲 👳 👷 👸 💂 🎅 👰 👼 💆 💇 🙍 🙎 🙅 🙆 💁 🙋 🙇 🙌 🚶 🏃 👯 💃 👫 👬 👭 💏 💑 👪'
+            .split(' ');
+        const emoji = emojiArr.map(v => {
+            return { text: v }
+        })
         const user_id = this.props.match.params.user;
         const Item = List.Item;
         const users = this.props.chat.users;
         if (!users[user_id]) {
             return null;
         }
-        const chatid = getChatId(user_id,this.props.user.user_id);
-        const chatmsgs = this.props.chat.chatmsg.filter(v=>{return getChatId(v.from,v.to)==chatid})
-        // console.log(chatmsgs)
+        const chatid = getChatId(user_id, this.props.user.user_id);
+        const chatmsgs = this.props.chat.chatmsg.filter(v => { return getChatId(v.from, v.to) == chatid })
         return (
             <div id="chat-page">
                 <NavBar
@@ -86,10 +99,39 @@ class Chat extends React.Component {
                                     this.setState({ text: v })
                                 }
                             }
-                            extra={<span onClick={() => { this.handleSumbit() }}>发送</span>}
+                            extra={
+                                <div>
+                                    <span
+                                        style={{ marginRight: 15 }}
+                                        onClick={
+                                            () => {
+                                                this.setState({ showEmoji: !this.state.showEmoji })
+                                                this.fixCarousel()
+                                            }
+                                        }
+                                    >
+                                        😀</span>
+                                    <span onClick={() => { this.handleSumbit() }}>发送</span>
+                                </div>
+                            }
                         >
                         </InputItem>
                     </List>
+                    {this.state.showEmoji ?
+                        <Grid
+                            data={emoji}
+                            columnNum={9}
+                            carouselMaxRow={4}
+                            isCarousel={true}
+                            onClick={el => {
+                                this.setState({
+                                    text: this.state.text + el.text
+                                })
+                            }}
+                        />
+                        : null
+                    }
+
                 </div>
             </div>
 
